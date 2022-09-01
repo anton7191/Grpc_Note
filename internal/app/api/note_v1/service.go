dabcd18417bbe68349342446c936b982eb782dab
anton <anton.dojnikov@spbpu.com>
2022-09-01 21:20:41 +0300

    change struck note

--- internal/app/api/note_v1/service.go
+++ internal/app/api/note_v1/service.go
@@ -2,10 +2,10 @@ package note_v1
 
 import desc "github.com/anton7191/note-server-api/pkg/note_v1"
 
-type Note struct {
+type Implementation struct {
 	desc.UnimplementedNoteV1Server
 }
 
-func NewNote() *Note {
-	return &Note{}
+func NewNote() *Implementation {
+	return &Implementation{}
 }
